16f05e19f20d081cde07ff0ffb26b44c182ac722
Tomas Garcia <tomasgarciadev@gmail.com>
2022-07-05 19:07:41 +0200

    Components folder created

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,7 +1,11 @@
 import {useRouter} from 'next/router'
 
 export default function EventPage() {
+  const router = useRouter();
+
   return (
-    <div>MyEvent</div>
+    <div>
+     <h1>MyEvent</h1>
+    </div>
   )
 }
